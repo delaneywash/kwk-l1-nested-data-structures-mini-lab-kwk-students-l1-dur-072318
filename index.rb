73f1cbc225049dd83d868dd1dec4d592afce9d6f
delaney washington <delaneywashington123@gmail.com>
2018-07-25 18:58:45 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -6,12 +6,13 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request:
+ingredients = [
   ["rye", "sourdough", "baguette"],
   ["ham", "salami", "turkey"],
   ["swiss", "munster", "cheddar"],
   ["mayo", "mustard", "tabasco"]
-end
+  
+ ]
 
 # example city_info hash
 cities = {
